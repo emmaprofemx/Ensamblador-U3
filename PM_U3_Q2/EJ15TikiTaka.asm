fb4ea702e5578de2dcc1b2294cec16be66ff72af
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-03-21 13:22:42 -0600

    PROYECTO RESTAURADO

---
+++ PM_U3_Q2/EJ15TikiTaka.asm
@@ -0,0 +1,31 @@
+TITLE NombrePrograma
+
+;DESCRPICIÓN 
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+;         Luna Sanchez Juan Pablo.
+;         Ruiz Garcia Emmanuel Alejandro.
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRPICIÓN
+
+INCLUDE Irvine32.inc  
+INCLUDE macros.inc
+.data
+; Área de Declaración de Variables
+
+;CONSTANTES
+acumulador dword 0d
+meses dword 12d
+.code
+	mainej15 PROC
+	
+		;Lógica del Programa	  
+		println "Cuanto vas a depositar?"
+		exit
+
+	mainej15 ENDP
+	
+	END mainej15
+
