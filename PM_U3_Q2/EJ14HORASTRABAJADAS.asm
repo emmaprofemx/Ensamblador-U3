6546aec21bcd5c8696d528c869a66ba84ad6bf1a
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-01 22:29:06 -0600

    Agregando problema 14
    -Ejercicio 14 de la unidad 3

---
+++ PM_U3_Q2/EJ14HORASTRABAJADAS.asm
@@ -0,0 +1,37 @@
+TITLE NombrePrograma
+
+;DESCRPICIÓN 
+;Objetivo: Archivo de Ejemplo
+;
+; Autore(s):
+;         Luna Sanchez Juan Pablo.
+;         Ruiz Garcia Emmanuel Alejandro.
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRPICIÓN
+
+INCLUDE Irvine32.inc  
+INCLUDE macros.inc
+.data
+; Área de Declaración de Variables
+
+;CONSTANTES
+acumulador dword 0d
+meses dword 12d
+.code
+	mainej14 PROC
+	
+		;Lógica del Programa	  
+		
+		;Una empresa tiene el registro de las horas que trabaja diariamente un empleado
+		;durante la semana(dias) y requiere determinat el total de estas , asi como el
+		;sueldo que recibira por las horas trabajadas
+
+
+
+		exit
+
+	mainej14 ENDP
+	
+	END mainej14
+
