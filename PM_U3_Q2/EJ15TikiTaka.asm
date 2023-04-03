102455b619f6809af60b12294dcc1c2312e1c7c4
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-03 17:38:22 -0600

    EJERCICIO 15
    PROGRAMA TERMINADO

--- PM_U3_Q2/EJ15TikiTaka.asm
+++ PM_U3_Q2/EJ15TikiTaka.asm
@@ -20,6 +20,8 @@ acumulador dword 0d
 venta dword 0d
 vueltas dword 0d
 catuno dword 0d
+catdos dword 0d
+cattres dword 0d
 .code
 	mainej15 PROC
 	
@@ -31,21 +33,37 @@ catuno dword 0d
 		 mov ecx, vueltas ; N CANTIDAD DE VUELTAS
 
 		 ciclo:
-			 println "Ingresa la venta: "
-			 call readint ;LEEMOS EAX
-			 cmp eax , 1000
-			 jl noMayor1000
-			 inc catuno
-			 noMayor1000:
+		     println "Ingresa la venta: "
+             call readint ;LEEMOS EAX
+             cmp eax, 1000 ; COMPARA CON 1000
+             jg ventaMayor1000
+             cmp eax, 500 ; COMPARA CON 500
+             jg ventaMayor500
+             inc cattres ; INCREMENTA SI ES MENOR O IGUAL A 500
+             jmp finCiclo
+
+             ventaMayor1000:
+             inc catuno ; INCREMENTA SI ES MAYOR A 1000
+             jmp finCiclo
+             
+             ventaMayor500:
+             inc catdos ; INCREMENTA SI ES MAYOR A 500 Y MENOR O IGUAL A 1000
+
+             finCiclo:
 			 loop ciclo
+         mov eax, catuno ; MOVEMOS EL VALOR PARA MOSTRARLO A PANTALLA
+         println "Ventas mayores a 1000: "
+         call writedec ; MOSTRANDO EL TOTAL DE VENTAS MAYORES A 1000
+         call crlf
+         mov eax, catdos ; MOVEMOS EL VALOR PARA MOSTRARLO A PANTALLA 
+         println "Ventas mayores a 500 y menores o iguales a 1000: "
+         call writedec ; MOSTRANDO EL TOTAL DE VENTAS MAYORES A 500 Y MENORES O IGUALES A 1000
+         call crlf
+         mov eax, cattres ; MOVEMOS EL VALOR PARA MOSTRARLO A PANTALLA 
+         println "Ventas menores o iguales a 500: "
+         call writedec ; MOSTRANDO EL TOTAL DE VENTAS MENORES O IGUALES A 500
+         call crlf
 
-		 mov eax , catuno
-		
-		call writedec ; MOSTRANDO EL TOTAL
-        
-
-
-		salir:
 		exit
 
 	mainej15 ENDP
