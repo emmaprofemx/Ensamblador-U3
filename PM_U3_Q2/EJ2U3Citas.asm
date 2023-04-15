c4b4cbfc9fb39ee68a6241f5d8c8c13778923735
Pablolunasa <113323847+PabloLunaSanchez@users.noreply.github.com>
2023-04-14 22:20:02 -0600

    Update EJ2U3Citas.asm
    Ejercicio 2

--- PM_U3_Q2/EJ2U3Citas.asm
+++ PM_U3_Q2/EJ2U3Citas.asm
@@ -1,6 +1,6 @@
 TITLE NombrePrograma
 
-;DESCRPICI”N 
+;DESCRPICI√ìN 
 ;Objetivo: Archivo de Ejemplo
 ;
 ; Autore(s):
@@ -8,26 +8,83 @@ TITLE NombrePrograma
 ;         Ruiz Garcia Emmanuel Alejandro.
 
 ; Semestre: 8vo Semestre ISC 
-;FIN DESCRPICI”N
+;FIN DESCRPICI√ìN
 
 INCLUDE Irvine32.inc  
-
+INCLUDE macros.inc
 .data
-; ¡rea de DeclaraciÛn de Variables
-
-
-
-var1 dword 100d
+; √Årea de Declaraci√≥n de Variables
+ numCita DWORD 0d
+    costoConsulta DWORD 0d
+    costoTotal DWORD 0d
 .code
+	mainej2 PROC
+		;L√≥gica del Programa	 
 
-	mainp22 PROC
-	
-		;LÛgica del Programa
-	    
-	
+		; Solicitar el sueldo al usuario
+   println "cuantas citas fueron?"
+    call ReadInt
+  
+    
+    ; Determinar el costo de la consulta seg√∫n el n√∫mero de cita
+    cmp eax, 3
+    jle costo200
+    cmp eax, 5
+    jle costo150
+    cmp eax, 8
+    jle costo100
+    jmp costo50
+    
+    ; Calcular el costo de la consulta y el costo total
+
+costo200:
+    mov costoConsulta, 200
+    mov ecx,costoConsulta ; aqui se respalda el costo de la consulta
+    mul costoConsulta
+    mov costoTotal,eax
+    jmp calcularCostoTotal
+
+costo150:
+mov costoConsulta, 150
+mov ecx,costoConsulta ; aqui se respalda el costo de la consulta
+    sub eax,3
+    mul costoConsulta
+    add eax, 600
+    mov costoTotal,eax
+    jmp calcularCostoTotal
+    
+costo100:
+mov costoConsulta, 100
+mov ecx,costoConsulta ; aqui se respalda el costo de la consulta
+    sub eax,5
+    mul costoConsulta
+    add eax, 900
+    mov costoTotal,eax
+    jmp calcularCostoTotal
+
+costo50:
+mov costoConsulta, 50
+mov ecx,costoConsulta ; aqui se respalda el costo de la consulta
+    sub eax,8
+    mul costoConsulta
+    add eax, 1200
+    mov costoTotal,eax
+
+    ; Calcular el costo total
+calcularCostoTotal:
+
+println "El costo de la cita es:"
+mov eax, ecx
+call WriteDec
+call crlf
+println"El costo del tratamiento es: "
+mov eax, costoTotal
+call WriteDec
+
 
 		exit
-		
-	mainp22 ENDP
+
+	mainej2 ENDP
 	
-	END mainp22
+	END mainej2
+
