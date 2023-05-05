33464d8ee2089fafa03bf1689804e10359fdfbd1
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-05-05 00:01:50 -0600

    Creacion de Menu -Calculadora
    Se hizo un menu en el cual despliega las opciones disponibles en la calculadora , y ademas se agrego un apartado para que el usuario pueda hacer varias operaciones , sin que se cierre automaticamente.

--- PM_U3_Q2/CALCULADORA.asm
+++ PM_U3_Q2/CALCULADORA.asm
@@ -15,16 +15,63 @@ INCLUDE macros.inc
 .data
 ; Área de Declaración de Variables
 
-
-;CONSTANTES
-
+opcion dword 0d
 .code
 
 	maincalcu01 PROC
 	
 		;Lógica del Programa
-		
+		operacion:
+	;-----------MENU----------------	
 	println "PROYECTO CALCULADORA"
-	maincalcu01 ENDP
+	println "1.-Suma"
+	println "2.-Resta"
+	println "3.-Multipliacion"
+	println "4.-Division"
+	println "SELECCIONA UNA DE LAS SIGUIENTES OPERACIONES"
+	;-------------------------------
+
+	;-----------LEYENDO OPCION----------------
+	call readint ; leemos la opcion eax
+	mov opcion , eax
+	;-----------------------------------------
+	
+	cmp opcion , 1
+	je suma
+
+	cmp opcion , 2
+	je resta
+
+	cmp opcion , 3
+	je multiplicacion
+
+	cmp opcion , 4
+	je division
+
+	suma:
+		println "Estas en la parte de suma"
+	jmp salir
+
+	resta:
+		println "Estas en la parte de resta"
+	jmp salir
+
+	multiplicacion:
+		println "Estas en la parte de multiplicacion"
+	jmp salir
+
+	division:
+		println "Estas en la parte de division"
+	jmp salir
+
+
+	salir:
+	; Preguntar si el usuario desea continuar
+		println "Desea continuar? (S/N)"
+		call readchar ; leemos la respuesta del usuario
+		cmp al , 'S' ; SI INGRESO LA S , VOLVEMOS AL MENU PRINCIPAL
+		je operacion ; Si el usuario desea continuar, saltamos a la etiqueta "operacion"
+
 	exit
+	maincalcu01 ENDP
 	END maincalcu01
